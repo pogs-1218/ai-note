Fix a gradient error.

from-scratch/linear_regression/housing_price/proj/main.py
@@ -1,11 +1,30 @@
+import numpy as np
 from utils import *
 
 data_file_path = './data/data.csv'
-features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 
-            'condition', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated']
-
-(train_data, train_label, test_data, test_label) = load_data(data_file_path, features)
-plot_features(train_data, train_label, features)
+#features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 
+#            'condition', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated']
+#
+#(train_data, train_label, test_data, test_label) = load_data(data_file_path, features)
+#plot_features(train_data, train_label, features)
 
 # As a result, I choose bathrooms, sqft_living, sqft_above.
+features = ['bathrooms', 'sqft_living', 'sqft_above']
+(x_train, y_train, x_test, y_test) = load_data(data_file_path, features)
 
+#data_file_path = './data/houses.csv'
+#(x_train, y_train) = load_data2(data_file_path)
+
+#print(f'y_train shape: {y_train.shape}')
+#print(f'x_train shape: {x_train.shape}')
+#print(f'{y_train[:3]}')
+#print(f'{x_train[:3]}')
+#x_train = x_train[:, 0:4:3]
+
+w_init = np.zeros((x_train.shape[1], 1))
+b_init = 0.0
+steps = 100
+learning_rate = 1e-7
+w_final, b_final, j_history = gradient_descent(w_init, b_init, x_train, y_train, learning_rate, steps)
+
+# todo: cost graph!

from-scratch/linear_regression/housing_price/proj/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import math
 
 def load_data(path, features):
@@ -17,26 +18,22 @@ def load_data(path, features):
   offset = math.ceil(len(df) * 0.8)
   train_df = df[:offset]
   test_df = df[offset:]
-  print(f"{len(train_df)}, {len(test_df)}")
+#print(f"{len(train_df)}, {len(test_df)}")
 
-  # Choose features, 
-  # At now, only select columns have a number.
-#features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_log', 'floors', 'waterfront', 'view', 
-#              'condition', 'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated']
   target = ['price']
 
   train_data = train_df.loc[:,features]
   train_label = train_df.loc[:,target]
-  print("train ================")
-  print(f"{type(train_data)}\n {train_data[:3]} ")
-  print(f"{train_label[:3]}")
+#  print("train ================")
+#  print(f"{type(train_data)}\n {train_data[:3]} ")
+#  print(f"{train_label[:3]}")
 
-  print("convert pd.DataFrame ot numpy.ndarray")
+#  print("convert pd.DataFrame ot numpy.ndarray")
   train_data = train_data.to_numpy()
   train_label = train_label.to_numpy()
-  print("train as numpy -----------")
-  print(f"{type(train_data)} with {train_data.shape}\n {train_data[:3]}")
-  print(f"{type(train_label)} with {train_label.shape}\n {train_label[:3]}")
+#  print("train as numpy -----------")
+#  print(f"{type(train_data)} with {train_data.shape}\n {train_data[:3]}")
+#  print(f"{type(train_label)} with {train_label.shape}\n {train_label[:3]}")
 
   test_data = test_df.loc[:,features]
   test_label = test_df.loc[:,target]
@@ -44,6 +41,12 @@ def load_data(path, features):
   test_label = test_label.to_numpy()
 
   return (train_data, train_label, test_data, test_label)
+
+def load_data2(path):
+  d = np.loadtxt(path, delimiter=",")
+  y_train = d[:,4]
+  x_train = d[:,:4]
+  return (x_train, y_train)
 
 def plot_features(data, label, features):  
 #xcount = 2
@@ -56,3 +59,76 @@ def plot_features(data, label, features):
     ax[i].set_xlabel(features[i])
   plt.show()
  
+
+def compute_cost(w, b, x_train, y_train):
+  '''
+    compute cost function
+    Prameters:
+      w
+      b
+      x_train
+      y_train
+
+    Return:
+      cost
+  '''
+  m = x_train.shape[0]  # may be 
+  n = x_train.shape[1]
+  fsum = 0
+  for i in range(m):
+    fwb = np.dot(x_train[i,:], w) + b
+    fsum += (fwb - y_train[i]) ** 2
+
+  cost = fsum / (2*m)
+  return cost
+
+
+def compute_gradient(w, b, x_train, y_train):
+  '''
+    Parameters:
+      w (n, 1)
+      b (m, 1)
+      x_train (m, n)
+      y_train (m, 1)
+    Returns:
+      dj_dw (n, 1)
+      dj_db scalar
+  '''
+  dj_dw = np.zeros_like(w)
+  dj_db = 0
+  m = x_train.shape[0]  # the number of training data
+  n = x_train.shape[1]  # the number of features
+
+  for i in range(m):
+    p = np.dot(x_train[i,:], w) + b
+    err = p - y_train[i]  
+    for j in range(n):
+      dj_dw[j] += err * x_train[i,j]
+    dj_db += err
+
+  dj_dw /= m
+  dj_db /= m
+
+  return (dj_dw, dj_db)
+
+def gradient_descent(w_init, b_init, x_train, y_train, alpha, steps):
+  '''
+    Parameters
+      w_init (n, 1)
+      b_init (m, 1)
+      x_train (m, n)
+      y_train (m, 1)
+  '''
+  w, b = w_init, b_init 
+  cost_history = []
+
+  for i in range(steps):
+    dj_dw, dj_db = compute_gradient(w, b, x_train, y_train)
+    w -= alpha * dj_dw
+    b -= alpha * dj_db
+    c = compute_cost(w, b, x_train, y_train)
+   
+    if i % 10 == 0:
+      print(f'{i} === {c}')
+
+  return (w, b, cost_history) 